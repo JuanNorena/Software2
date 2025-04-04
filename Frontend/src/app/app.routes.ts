0720696a9bc2786be37e09e5635f2de9d5e703aa
Daniela <daniela.munoza1@uqvirtual.edu.co>
2025-04-04 17:18:57 -0500

    Login Front

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './componentes/login/login.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{path: 'login', component: LoginComponent},
+{path: '**', redirectTo: '/login', pathMatch: 'full'},
+
+];
